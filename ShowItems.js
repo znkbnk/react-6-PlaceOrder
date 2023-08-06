85d89b64607661893d12ccb16dcecda06bb8cc44
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-06 19:41:49 +0100

    Create ShowItems.js

---
+++ ShowItems.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ShowItems = () => {
+  // State to store the list of items and their quantities
+
+  // Function to update the quantity of an item
+
+  return (
+    <div className='show-items'>
+      {/* Map through the list of items and render them */}
+      {/* Display item name, price, and an input field for quantity */}
+    </div>
+  );
+};
+
+export default ShowItems;
