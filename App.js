8107e9a0d74099e62529a0dea41bffbae44c2a09
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-08 19:46:48 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,22 @@
+// src/App.js
 import React from 'react';
-import './App.css';
-import ShowItems from './PlaceOrder/ShowItems';
-import Payments from './PlaceOrder/Payments';
-import Address from './PlaceOrder/Address';
-import PlaceOrderAction from './PlaceOrder/PlaceOrderAction';
+import './App.css'; // You can include your CSS styles here
+import PlaceOrderScreen from './components/PlaceOrderScreen';
 
 function App() {
-  // State to store the list of items and their quantities
-  // State to store the selected payment method
-  // State to store the user's shipping address
-
-  // Function to update the quantity of an item
-  // Function to handle payment method selection
-  // Function to handle form input changes
-  // Function to handle the order placement and call the API
-
   return (
-    <div className="app">
-      <h1>Place Order</h1>
-      <ShowItems />
-      <Payments />
-      <Address />
-      <PlaceOrderAction
-        items={/* pass the items state */}
-        selectedPayment={/* pass the selected payment state */}
-        shippingAddress={/* pass the shipping address state */}
-        onPlaceOrder={/* pass the function to handle order placement */}                            
-      />
-    </div>         
+    <div className="App">
+      <header className="App-header">
+        <h1>Place Order App</h1>
+      </header>
+      <main className="App-main">
+        <PlaceOrderScreen />
+      </main>
+      <footer className="App-footer">
+        <p>&copy; {new Date().getFullYear()} Place Order App</p>
+      </footer>
+    </div>
   );
 }
 
-export default App;                         
+export default App;
